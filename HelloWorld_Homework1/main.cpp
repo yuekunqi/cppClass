8f4ebb0100a8bf5a297bcc19bda22e0e1e33a117
yuekunqi <648846955@qq.com>
2013-12-16 23:42:59 +0800

    Revert "All Homeworks"
    This reverts commit 966764b488e671ed78200d0b10bc1e25adfb6714.

--- HelloWorld_Homework1/main.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	cout<<"Hello World!\nby H.Fly.Colin(2012060090014)"<<endl;
-	return 0;
-}
--- Homework4/power.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int power(int x,int y){
-	int ans;
-	if(y==1) ans=x;
-	else ans = x*power(x,y-1);
-	return ans;
-}
